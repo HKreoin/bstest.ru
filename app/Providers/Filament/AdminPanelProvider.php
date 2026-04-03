63a644d445a803a21771cd006820a89f71d27101
Ilshat Khabirov <khabdev@yandex.ru>
2026-04-03 09:24:12 +0300

    redirect to tests from logo from admin dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,6 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->homeUrl(fn () => route('tests.index'))
             ->authGuard('web')
             ->login()
             ->profile()
